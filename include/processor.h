5892b2fbfe4e93c39ab9d0160636986f3fa71a4c
Brenda Camacho <brenda.camacho@tortoise.dev>
2021-01-16 16:52:10 -0600

    add linuxparser to Processor class

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,30 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+class CPUData
+{
+public:
+      int user{0};
+      int nice{0};
+      int system{0};
+      int idle{0};
+      int iowait{0};
+      int irq{0};
+      int softirq{0};
+      int steal{0};
+      int guest{0};
+      int guest_nice{0};
+      int Idle{0};
+      int NonIdle{0};
+      int Total{0};
+      void GetDataFromString();
+};
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
-
-  // TODO: Declare any necessary private members
  private:
+  CPUData prev_, current_;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,31 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+    int totald;
+    int idled;
+    current_.GetDataFromString();
+    totald = current_.Total - prev_.Total;
+    idled = current_.Idle - prev_.Idle;
+    prev_ = current_;
+    return (totald - idled)/(totald * 1.0);
+}
+
+void CPUData::GetDataFromString()
+{
+    std::vector<std::string> cpuDataStr = LinuxParser::CpuUtilization();
+    user = std::stoi(cpuDataStr.at(1));
+    nice = std::stoi(cpuDataStr.at(2));
+    system = std::stoi(cpuDataStr.at(3));
+    idle = std::stoi(cpuDataStr.at(4));
+    iowait = std::stoi(cpuDataStr.at(5));
+    irq = std::stoi(cpuDataStr.at(6));
+    softirq = std::stoi(cpuDataStr.at(7));
+    steal = std::stoi(cpuDataStr.at(8));
+    guest = std::stoi(cpuDataStr.at(9));
+    guest_nice = std::stoi(cpuDataStr.at(10));
+    Idle = idle + iowait;
+    NonIdle = user + nice +system + irq + softirq +steal;
+    Total = Idle + NonIdle;
+}
