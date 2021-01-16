b169074e969561aa1a2483838abacd7cc84c861d
Brenda Camacho <brenda.camacho@tortoise.dev>
2021-01-16 16:07:06 -0600

    Format namespace

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,18 @@
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  std::stringstream hh,mm,ss;
+  std::string time;
+  int h, m, s;
+  h = (seconds) / pow(60,2);
+  hh << std::setw(2) << std::setfill('0') << h;
+  time += hh.str() + ':';
+  m = (seconds % (60*60))/60;
+  mm << std::setw(2) << std::setfill('0') << m;
+  time += mm.str() + ':';
+  s = seconds%60;
+  ss << std::setw(2) << std::setfill('0') << s;
+  time += ss.str();
+  return time;
+}
--- src/system.cpp
+++ src/system.cpp
@@ -34,17 +34,12 @@ vector<Process>& System::Processes() {
 
 std::string System::Kernel() { return kernel_; }
 
-// TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// TODO: Return the operating system name
 std::string System::OperatingSystem() { return os_; }
 
-// TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() {return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return LinuxParser::UpTime(); }
